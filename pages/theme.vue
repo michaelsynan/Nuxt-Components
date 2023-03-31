f9bb262799c7300e16731de46c0bc3a319788550
mikey <mforest@protonmail.com>
2023-03-31 17:34:23 -0400

    Adding theme page

---
+++ pages/theme.vue
@@ -0,0 +1,60 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="grid grid-cols-3 mt-40 max-w-8xl mx-auto justify-center align-center justify-items-center">
+    <div class="col-span-3 text-6xl font-bold left-0 pb-20">Theme Design System</div>
+  <div>
+    <h2>Theme Colors</h2>
+    <div>
+      <p>Theme colors go here</p>
+    </div>
+    <button class="px-3 py-1.5 my-4 bg-red-800 hover:bg-red-700 text-stone-50">Generate Colors</button>
+    <div>
+      <h3>Color Variants</h3>
+      <ul>
+        <li>Primary</li>
+        <li>SecondaryA</li>
+        <li>SecondaryB</li>
+        <li>SecondaryC</li>
+        <li>SecondaryD</li>
+      </ul>
+    </div>
+  </div>
+  <div>
+    <h2>Theme Fonts</h2>
+    <div>
+      <p>Theme colors go here</p>
+    </div>
+    <button class="px-3 py-1.5 my-4 bg-red-800 hover:bg-red-700 text-stone-50">Generate Fonts</button>
+    <div>
+      <h3>Color Variants</h3>
+      <ul>
+        <li>Display</li>
+        <li>Body</li>
+      </ul>
+    </div>
+  </div>
+  <div>
+    <h2>Button Style</h2>
+    <div>
+      <p>Theme colors go here</p>
+    </div>
+    <button class="px-3 py-1.5 my-4 bg-red-800 hover:bg-red-700 text-stone-50">Generate Buttons</button>
+    <div>
+      <h3>Button Elements</h3>
+      <ul>
+        <li>Padding</li>
+        <li>Font Size</li>
+        <li>Capitalization</li>
+        <li>Radius</li>
+        <li>Border</li>
+        <li>Shadow Color</li>
+        <li>Shadow Blur</li>
+      </ul>
+    </div>
+  </div>
+  </div>
+</template>
+
+
+<style scoped></style>
